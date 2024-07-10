5281db10aae70dbd64d0b3cfc2182fff40ccfbf9
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-07-10 16:19:17 +0200

    añadido método handler

--- src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,5 +1,14 @@
 package com.laura.curso.springboot.webapp.springboot_web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/details")
+    public String details(){
+        return "details";
+    }
 
 }
