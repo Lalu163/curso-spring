9e81a6dae3ebca1c3b4157801a859250e5f17d54
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-07-23 22:17:23 +0200

    llamada del model al restcontroller

--- src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.laura.curso.springboot.webapp.springboot_web.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
     public Map <String, Object> details(){
+        User user = new User("Laura", "Contreras");
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola Mundo Spring Boot");
-        body.put("name", "Laura");
-        body.put("lastname", "Contreras");
+        body.put("user", user);
+        
         return body;
     }
 
