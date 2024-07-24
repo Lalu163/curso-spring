c9855aea90ba8d28da4396c68e852db313fa874d
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-07-24 14:19:49 +0200

    uso de dto en el restcontroller

--- src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -8,13 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.laura.curso.springboot.webapp.springboot_web.models.User;
+import com.laura.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map <String, Object> details(){
+    public UserDto details(){
+        User user = new User("Laura", "Contreras");
+        
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+        
+        return userDto;
+    }
+
+    @GetMapping("/details-map")
+    public Map <String, Object> detailsMap(){
         User user = new User("Laura", "Contreras");
         Map<String, Object> body = new HashMap<>();
 
