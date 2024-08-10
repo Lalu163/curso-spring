67c3f13777abcfb643220f2e8107e1ac6fe0ff16
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-08-10 23:19:37 +0200

    uso de la anotación ModelAttribute para reutilzar datos

--- src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.laura.curso.springboot.webapp.springboot_web.models.User;
 
@@ -26,14 +27,16 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-        List<User> users = Arrays.asList(new User("Laia", "Perez", "laia@correo.com"),
-            new User("Pedro", "Fernandez"),
-            new User("Marisa", "Salas", "marisa@correo.com"));
-
-        model.addAttribute("users", users);
+        
         model.addAttribute("title", "Listado de usuarios");
         return "list";
     }
-    
+
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        return Arrays.asList(new User("Laia", "Perez", "laia@correo.com"),
+        new User("Pedro", "Fernandez"),
+        new User("Marisa", "Salas", "marisa@correo.com"));
+    }
 
 }
