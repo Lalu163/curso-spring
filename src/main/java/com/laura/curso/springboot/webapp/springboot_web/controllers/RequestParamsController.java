cc345348857c68da500d99497efbbb0b5d8406e9
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-08-14 18:13:58 +0200

    creacion paramDto

--- src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/laura/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -1,5 +1,22 @@
 package com.laura.curso.springboot.webapp.springboot_web.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.laura.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
 public class RequestParamsController {
+
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam String message) {
+
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
 
 }
